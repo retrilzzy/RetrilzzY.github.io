46d647607de5a48e7669ad9c8695a821e5736b7e
Razy <129707458+RetrilzzY@users.noreply.github.com>
2024-03-16 15:57:55 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+(window.setScroll = () => document.body.style.setProperty('--scroll', scrollY / innerHeight))();
+['scroll', 'resize'].forEach(e => addEventListener(e, setScroll));
+
+const bg = document.querySelector('#bg');
+
+addEventListener('touchstart', () => bg.style.setProperty('--multiplier', '0'));
+addEventListener('mousemove', ({ clientX, clientY }) => {
+    bg.style.setProperty('--tx', `${20 * (clientX - innerWidth / 2) / innerWidth}px`);
+    bg.style.setProperty('--ty', `${20 * (clientY - innerHeight / 2) / innerHeight}px`);
+});
+
+['mouseenter', 'mouseleave'].forEach(e => document.addEventListener(e, () => {
+    if (e === 'mouseleave') bg.removeAttribute('style');
+    bg.style.transition = 'transform .1s linear';
+    setTimeout(() => bg.style.transition = '', 100);
+}));
+
+document.querySelector('#arrow svg').addEventListener('click', () => {
+    const start = performance.now();
+
+    !function step() {
+        const progress = (performance.now() - start) / 200;
+        scrollTo({ top: (innerWidth > 780 ? .3 : .8) * innerHeight * easeOutCubic(progress) });
+        if (progress < 1) requestAnimationFrame(step);
+    }();
+
+    function easeOutCubic(x) {
+        return 1 - Math.pow(1 - x, 3);
+    }
+});
