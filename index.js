799a62b0e95c89f0f1ebe8df22dfd7026012c48b
RetrilzzY <retrilzzy@gmail.com>
2024-04-16 22:24:07 +0500

    add frieren bg image

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ document.querySelector('#arrow svg').addEventListener('click', () => {
 });
 
 // random bg
-var images = ['/bg0.webp', '/bg1.webp', '/bg2.webp', '/bg3.jpg', '/bg4.jpg', '/bg5.jpg' , '/bg6.jpg'];
+var images = ['/bg0.webp', '/bg1.webp', '/bg2.webp', '/bg3.jpg', '/bg4.jpg', '/bg5.jpg' , '/bg6.jpg', '/bg7.webp'];
 
-var randomImage = Math.floor(Math.random() * 7);
+var randomImage = Math.floor(Math.random() * 8);
 
 $(document).ready(function() {
   $("#bg").css("background-image", "url('/backgrounds" + images[randomImage] + "')");
